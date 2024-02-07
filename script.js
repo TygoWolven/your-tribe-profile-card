68b9f46dce8ad391a2b9668c688cf54479f71aad
TygoWolven <144010858+TygoWolven@users.noreply.github.com>
2024-02-07 11:09:49 +0100

    .

--- script.js
+++
@@ -1,9 +0,0 @@
-const Arrowbutton = document.querySelector("button");
-const Tygo = document.querySelector("body > img:nth-of-type(1)");
-
-Arrowbutton.addEventListener("click", trippen);
-
-function trippen(){
-    Tygo.classList.toggle("spin")
-}
-
